bd1017ae82c3fcbdbf13c82443dcc82c54cfbb7b
Tom <tombeugel@gmail.com>
2019-10-12 20:12:35 +0200

    _HBO-ICTmemes_-2019-10-12.sql

---
+++ src/sql/_HBO-ICTmemes_-2019-10-12.sql
@@ -0,0 +1,310 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2019 at 11:50 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hbo-ictmemes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment-ID` int(10) UNSIGNED NOT NULL,
+  `meme-ID` int(10) UNSIGNED NOT NULL,
+  `user-ID` int(10) UNSIGNED NOT NULL,
+  `inhoud` varchar(500) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emailverificatie`
+--
+
+CREATE TABLE `emailverificatie` (
+  `user-ID` int(10) UNSIGNED NOT NULL,
+  `usermail` varchar(50) COLLATE utf8_bin NOT NULL,
+  `verificatiecode` int(10) UNSIGNED NOT NULL,
+  `geverifieerd` tinyint(1) NOT NULL DEFAULT 0,
+  `geverifieerd_door` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `errors`
+--
+
+CREATE TABLE `errors` (
+  `flag-ID` int(10) UNSIGNED NOT NULL,
+  `meme-ID` int(10) UNSIGNED DEFAULT NULL,
+  `comment-ID` int(10) UNSIGNED DEFAULT NULL,
+  `snitch-ID` int(10) UNSIGNED DEFAULT NULL,
+  `boef-ID` int(10) UNSIGNED DEFAULT NULL,
+  `datum` date NOT NULL,
+  `errortype` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meme`
+--
+
+CREATE TABLE `meme` (
+  `meme-ID` int(10) UNSIGNED NOT NULL,
+  `meme-titel` varchar(30) COLLATE utf8_bin NOT NULL,
+  `user-ID` int(10) UNSIGNED NOT NULL,
+  `datum` date NOT NULL,
+  `tagnaam` varchar(20) COLLATE utf8_bin NOT NULL,
+  `locatie` varchar(200) COLLATE utf8_bin NOT NULL,
+  `school` varchar(50) COLLATE utf8_bin NOT NULL,
+  `upvote` smallint(5) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rollen`
+--
+
+CREATE TABLE `rollen` (
+  `userrole` varchar(20) COLLATE utf8_bin NOT NULL,
+  `user-ID` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school`
+--
+
+CREATE TABLE `school` (
+  `schoolnaam` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `support`
+--
+
+CREATE TABLE `support` (
+  `support-ID` int(10) UNSIGNED NOT NULL,
+  `onderwerp` varchar(30) COLLATE utf8_bin NOT NULL,
+  `inhoud` varchar(500) COLLATE utf8_bin NOT NULL,
+  `datum` date NOT NULL,
+  `opgelost` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `tagnaam` varchar(30) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user-ID` int(10) UNSIGNED NOT NULL,
+  `usermail` varchar(100) COLLATE utf8_bin NOT NULL,
+  `naam` varchar(30) COLLATE utf8_bin NOT NULL,
+  `wachtwoord` varchar(200) COLLATE utf8_bin NOT NULL,
+  `vorig_wachtwoord` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `profile_picture` varchar(300) COLLATE utf8_bin DEFAULT NULL,
+  `schoolnaam` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `laatste_login` datetime DEFAULT NULL,
+  `aantal_foute_logins` tinyint(3) UNSIGNED DEFAULT 0,
+  `userrole` varchar(20) COLLATE utf8_bin NOT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT 0,
+  `gebanned` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment-ID`),
+  ADD KEY `meme-ID` (`meme-ID`),
+  ADD KEY `user-ID` (`user-ID`);
+
+--
+-- Indexes for table `emailverificatie`
+--
+ALTER TABLE `emailverificatie`
+  ADD PRIMARY KEY (`user-ID`),
+  ADD KEY `usermail` (`usermail`),
+  ADD KEY `geverifieerd_door` (`geverifieerd_door`),
+  ADD KEY `geverifieerd` (`geverifieerd`);
+
+--
+-- Indexes for table `errors`
+--
+ALTER TABLE `errors`
+  ADD PRIMARY KEY (`flag-ID`),
+  ADD KEY `meme-ID` (`meme-ID`),
+  ADD KEY `comment-ID` (`comment-ID`),
+  ADD KEY `snitch-ID` (`snitch-ID`),
+  ADD KEY `boef-ID` (`boef-ID`);
+
+--
+-- Indexes for table `meme`
+--
+ALTER TABLE `meme`
+  ADD PRIMARY KEY (`meme-ID`),
+  ADD KEY `meme-titel` (`meme-titel`),
+  ADD KEY `meme-titel_2` (`meme-titel`),
+  ADD KEY `meme-titel_3` (`meme-titel`),
+  ADD KEY `user-ID` (`user-ID`),
+  ADD KEY `tagnaam` (`tagnaam`),
+  ADD KEY `school` (`school`);
+
+--
+-- Indexes for table `rollen`
+--
+ALTER TABLE `rollen`
+  ADD PRIMARY KEY (`userrole`),
+  ADD KEY `user-ID` (`user-ID`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`schoolnaam`);
+
+--
+-- Indexes for table `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`support-ID`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tagnaam`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user-ID`),
+  ADD KEY `usermail` (`usermail`),
+  ADD KEY `userrole` (`userrole`),
+  ADD KEY `schoolnaam` (`schoolnaam`),
+  ADD KEY `is_verified` (`is_verified`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment-ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `meme`
+--
+ALTER TABLE `meme`
+  MODIFY `meme-ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `support`
+--
+ALTER TABLE `support`
+  MODIFY `support-ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user-ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`meme-ID`) REFERENCES `meme` (`meme-ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user-ID`) REFERENCES `user` (`user-ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `emailverificatie`
+--
+ALTER TABLE `emailverificatie`
+  ADD CONSTRAINT `emailverificatie_ibfk_1` FOREIGN KEY (`geverifieerd_door`) REFERENCES `user` (`user-ID`) ON DELETE SET NULL,
+  ADD CONSTRAINT `emailverificatie_ibfk_2` FOREIGN KEY (`user-ID`) REFERENCES `user` (`user-ID`),
+  ADD CONSTRAINT `emailverificatie_ibfk_3` FOREIGN KEY (`usermail`) REFERENCES `user` (`usermail`);
+
+--
+-- Constraints for table `errors`
+--
+ALTER TABLE `errors`
+  ADD CONSTRAINT `errors_ibfk_1` FOREIGN KEY (`snitch-ID`) REFERENCES `user` (`user-ID`),
+  ADD CONSTRAINT `errors_ibfk_2` FOREIGN KEY (`boef-ID`) REFERENCES `user` (`user-ID`),
+  ADD CONSTRAINT `errors_ibfk_3` FOREIGN KEY (`comment-ID`) REFERENCES `comments` (`comment-ID`),
+  ADD CONSTRAINT `errors_ibfk_4` FOREIGN KEY (`meme-ID`) REFERENCES `meme` (`meme-ID`);
+
+--
+-- Constraints for table `meme`
+--
+ALTER TABLE `meme`
+  ADD CONSTRAINT `meme_ibfk_1` FOREIGN KEY (`user-ID`) REFERENCES `user` (`user-ID`),
+  ADD CONSTRAINT `meme_ibfk_2` FOREIGN KEY (`tagnaam`) REFERENCES `tags` (`tagnaam`),
+  ADD CONSTRAINT `meme_ibfk_3` FOREIGN KEY (`school`) REFERENCES `school` (`schoolnaam`);
+
+--
+-- Constraints for table `rollen`
+--
+ALTER TABLE `rollen`
+  ADD CONSTRAINT `rollen_ibfk_1` FOREIGN KEY (`user-ID`) REFERENCES `user` (`user-ID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`schoolnaam`) REFERENCES `school` (`schoolnaam`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`is_verified`) REFERENCES `emailverificatie` (`geverifieerd`),
+  ADD CONSTRAINT `user_ibfk_3` FOREIGN KEY (`userrole`) REFERENCES `rollen` (`userrole`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
