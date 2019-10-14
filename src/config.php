883a1aa6705ec220bd842b9593d91949a6425288
RedFirebreak <sjilderda@gmail.com>
2019-10-14 12:14:19 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,4 +1,8 @@
 <?php
-$websiteroot = "\HBO-ICTMemes[develop]";
+  \\Set your own documentroot to start working. If you dont, everything will look fucked up cause dynamic css paths
+  $websiteroot = "\HBO-ICTMemes[develop]";
 
+  \\ This is the webservers website root. Comment this outto start working on your changes
+  \\ Yes this will overwrite a current documentroot. Hence why you need to comment this one out during development
+  $websiteroot = "\subdomain\develop";
 ?>
