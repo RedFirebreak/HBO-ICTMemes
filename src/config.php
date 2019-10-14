af69163e58b5cdc3a6fd29dd9a73838a5693b791
Stefan Jilderda <sjilderda@gmail.com>
2019-10-14 12:16:26 +0200

    Well, i fucked that up lol

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
-  \\Set your own documentroot to start working. If you dont, everything will look fucked up cause dynamic css paths
+  //Set your own documentroot to start working. If you dont, everything will look fucked up cause dynamic css paths
   $websiteroot = "\HBO-ICTMemes[develop]";
 
-  \\ This is the webservers website root. Comment this outto start working on your changes
-  \\ Yes this will overwrite a current documentroot. Hence why you need to comment this one out during development
+  // This is the webservers website root. Comment this outto start working on your changes
+  // Yes this will overwrite a current documentroot. Hence why you need to comment this one out during development
   $websiteroot = "\subdomain\develop";
 ?>
