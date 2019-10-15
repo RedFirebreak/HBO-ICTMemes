89b348347ca63c3e8923895e1c9bee29ff1de328
Koen Sikkema <koensikkema@hotmail.com>
2019-10-15 21:30:28 +0200

    form.changepassword.php

--- account/form.changepassword.php
+++ account/form.changepassword.php
@@ -13,3 +13,18 @@
     <!-- Start coding here! :D -->
 
 <!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
+	<title>change password</title>
+	</head>
+  <body>
+<form changepassword "/form.changepassword.php">
+	Old password:<br>
+	<input type="text" name="Old password" value="Type old password">
+	<br><br>
+	New password:<br>
+	<input type="text" name="New password" value="Type New password">
+	<br><br>
+	New password again:<br>
+	<input type="text" name="New password" value="Type New password again">
+	<br><br>
+	<input type="submit" value="Change password">
+</form>
