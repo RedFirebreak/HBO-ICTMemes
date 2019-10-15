eb3091b55d2427704aa01f4f5f39639507813109
Tom <tombeugel@gmail.com>
2019-10-15 21:46:18 +0200

    form.upload.php

--- account/form.reset.php
+++ account/form.reset.php
@@ -11,5 +11,21 @@
 ?>
 
     <!-- Start coding here! :D -->
+	<head>
+		<meta charset="utf-8">
+		<title>Password Reset</title>
+	</head>
+	
+	<body>
+		
+		<form action= "/reset.php" method="post">
+		<p>In order to regain acces to your account, please enter your e-mailadress down below.<br>
+		An e-mail will be sent that will contain a new password.
+		</p>
+		E-mail:<input type="text" name="ëmail">
+		<input type="submit" value="send">
+		</form>
+		
+	</body>
 
 <!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
--- upload/form.upload.php
+++ upload/form.upload.php
@@ -11,5 +11,28 @@
 ?>
 
     <!-- Start coding here! :D -->
+	<head>
+		<meta charset="utf-8">
+		<title>Upload</title>
+	</head>
+	
+	<body>
+		
+		<form action="/upload.php" method="post">
+		<p>Insert your meme here:</p>
+		<input type="text" name="meme">
+		<p>Insert the name of your meme:</p>
+		<input type="text" name="name">
+		<p>Select the tags for your meme:</p>
+		<select name="tags">
+			
+		</select>
+		</form>
+		<?php
+		/*handige manier om tags te selecteren is nog nodig
+		ook belangrijk: niet vergeten te registreren van welke school de user komt
+		*/
+		?>
+	</body>
 
 <!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
