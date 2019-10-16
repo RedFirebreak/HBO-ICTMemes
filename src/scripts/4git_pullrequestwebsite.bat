826b5883f116aea76d43704ac7e4a7f2346e4642
sjilderdaspam <sjilderdaspam@gmail.com>
2019-10-16 22:59:01 +0200

    Pull request?

---
+++ src/scripts/4git_pullrequestwebsite.bat
@@ -0,0 +1,13 @@
+@echo off  
+  echo Uploading/Pushing YOUR fork of HBO-ICTMemes. . .
+  echo.
+  echo ------------------------------------------
+    cd "..\..\"
+    git add .
+    set /p push-name="Enter comment for your fork: "
+    git commit -m "%push-name%"
+    git push upstream/develop
+  echo ------------------------------------------
+  echo.
+pause
+exit
