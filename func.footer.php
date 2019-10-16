3178ccfe61d20d36877767d49d3f621548985bb6
RedFirebreak <sjilderda@gmail.com>
2019-10-16 13:16:06 +0200

    DATABASE CONNECTION HYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYPE

--- func.footer.php
+++ func.footer.php
@@ -14,3 +14,6 @@
     <script>window.jQuery || document.write('<script src="/src/js/jquery.min.js"><\/script>')</script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="/src/js/bootstrap.min.js"></script>
+    <?php
+        databaseDisconnect($dbConnection); // disconnect from database
+    ?>
