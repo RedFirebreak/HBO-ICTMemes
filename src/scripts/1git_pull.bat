635228d0cdbb46c1b7bf2d2c9bb4d901269ae03b
Stefan Jilderda <sjilderda@gmail.com>
2019-10-16 22:43:00 +0200

    test

---
+++ src/scripts/1git_pull.bat
@@ -0,0 +1,11 @@
+@echo off
+  
+  echo Updating YOUR fork of HBO-ICTMemes. . .
+  echo.
+  echo ------------------------------------------
+    cd "..\..\"
+    git pull
+  echo ------------------------------------------
+  echo.
+pause
+exit
