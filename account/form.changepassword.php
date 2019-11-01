1fe9f3f15195057bf1756ac6df920f4786dd8193
janine <janine00.dijkstra@gmail.com>
2019-11-01 11:13:40 +0100

    Update form.changepassword.php
    check of iemand zijn wachtwoord wil veranderen, zet ik erin zodra ik de change personal detail functionaliteit heb

--- account/form.changepassword.php
+++ account/form.changepassword.php
@@ -1,30 +1,52 @@
-
 <?php
-    /*
-        [DESCRIPTION]
-        This file does (something).
+$password = $_POST['oldpassword'];
+$newpassword = $_POST['newpassword'];
+$newpassword2 = $_POST['newpassword2'];
+$email = $_POST['email'];
 
-        [INFO]
-        Author:     Stef
-        Date:       11-10-2019
-    */
+$sql = "UPDATE user SET wachtwoord = '$newpassword' WHERE usermail = '$email'"; //sql query voor veranderen oud wachtwoord
+$sql2 = "UPDATE user SET vorig_wachtwoord = $password WHERE usermail = $email"; //sql query voor doorschuiven oud wachtwoord
+$sql3 = "SELECT `wachtwoord` FROM `user` WHERE usermail = '$email'"; //sql query voor checken wachtwoord
+$result = $dbConnection->query($sql);
+$result2 = $dbConnection->query($sql2);
+$dbpassword = $dbConnection->query($sql3);
+
+if ($loggedin) {
+ 	if ($password == $dbpassword) {
+  		$result; //verander password in het nieuwe password
+  		$result2; // schuif het vorige password door naar de vorige_wachtwoord kolom in database
+  		echo 'Your update has been saved';
+	}
+ 	else {
+  		echo 'The filled in password does not seem to match your password';
+ 	}
+}
+else {
+ 	echo 'Please log in to change your password'
+}
+
+
+
 ?>
 
-    <!-- Start coding here! :D -->
-
-<!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
-	<title>change password</title>
-	</head>
-  <body>
+<!-- Start coding here! :D -->
+ <!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
+ <title>change password</title>
+ </head>
+  <body>
 <form changepassword "/form.changepassword.php">
-	Old password:<br>
-	<input type="text" name="Old password" value="Type old password">
-	<br><br>
-	New password:<br>
-	<input type="text" name="New password" value="Type New password">
-	<br><br>
-	New password again:<br>
-	<input type="text" name="New password" value="Type New password again">
-	<br><br>
-	<input type="submit" value="Change password">
+ Email address:<br>
+ <input type="text" name="email">
+ <br>
+ Old password:<br>
+ <input type="text" name="oldpassword">
+ <br>
+ New password:<br>
+ <input type="text" name="newpassword">
+ <br>
+ New password again:<br>
+ <input type="text" name="newpassword2">
+ <br>
+ <input type="submit" name="submit">
 </form>
+</body>
