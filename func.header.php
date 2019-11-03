b32883d4068231e8ba90f83fc8c9ab57909b9d5e
RedFirebreak <sjilderda@gmail.com>
2019-11-03 20:38:27 +0100

    i forgot to push this lol

--- func.header.php
+++ func.header.php
@@ -76,28 +76,56 @@
 	<header>
 		<div class="cd-logo"><a href="/"><img src="/src/img/cd-logo.svg" alt="Logo"></a></div>
 
-		<nav class="cd-main-nav-wrapper">
-			<ul class="cd-main-nav">
-				<li><a href="/">Homepage</a></li>
-				<li><a href="/upload/">Upload</a></li>
-				<li><a href="/tags.php">Tags</a></li>
-				<li>
-					<a href="#0" class="cd-subnav-trigger"><span>Account</span></a>
 
-					<ul>
-						<li class="go-back"><a href="#0">Menu</a></li>
-						<li><a href="/account/login.php">Log-in</a></li>
-						<li><a href="/account/">Account</a></li>
-						<li><a href="/upload/">Upload</a></li>
-            <li><a href="/support.php">Support</a></li>
-            <li><a style="color: red;" href="?logout='1'">Log-uit</a></li>
-						<li><a href="#0"  class="placeholder">Placeholder</a></li>
-					</ul>
-				</li>
-			</ul> <!-- .cd-main-nav -->
-		</nav> <!-- .cd-main-nav-wrapper -->
-		
-		<a href="#0" class="cd-nav-trigger"><span></span></a>
+
+          <?php
+          if ($Loggedin) {
+            // Show the menu for logged in people
+                echo "<nav class='cd-main-nav-wrapper'>";
+                echo "<ul class='cd-main-nav'>";
+                  echo "<li><a href='/'>Homepage</a></li>";
+                  echo "<li><a href='/upload/'>Upload</a></li>";
+                  echo "<li><a href='/tags.php'>Tags</a></li>";
+                    echo "<li>";
+                    echo "<a href='#0' class='cd-subnav-trigger'><span>Account</span></a>";
+                    echo "<ul>";
+                  echo "<li class='go-back'><a href='#0'>Menu</a></li>";
+
+                  echo "<li><a href='/account/'>Account</a></li>";
+                  echo "<li><a href='/upload/'>Upload</a></li>";
+                  echo "<li><a href='/support.php'>Support</a></li>";
+                  echo "<li><a style='color: red;' href='?logout=1'>Log-uit</a></li>";
+                  echo "<li><a href='#'  class='placeholder'>Placeholder</a></li>";
+                echo "</ul>";
+                echo "</li>";
+                echo "</ul> <!-- .cd-main-nav -->";
+                echo "</nav> <!-- .cd-main-nav-wrapper -->";
+            
+                echo "<a href='#0' class='cd-nav-trigger'><span></span></a>";
+          } // End menu for logged in people 
+          else {
+            // Show menu for not-logged in people
+            echo "<nav class='cd-main-nav-wrapper'>";
+            echo "<ul class='cd-main-nav'>";
+              echo "<li><a href='/'>Homepage</a></li>";
+              echo "<li><a href='/upload/'>Upload</a></li>";
+              echo "<li><a href='/tags.php'>Tags</a></li>";
+                echo "<li>";
+                echo "<a href='#0' class='cd-subnav-trigger'><span>Account</span></a>";
+                echo "<ul>";
+              echo "<li class='go-back'><a href='#0'>Menu</a></li>";
+              echo "<li><a href='/account/login.php'>Log-in</a></li>";
+              echo "<li><a href='/account/register.php'>Registreren</a></li>";
+              echo "<li><a href='#'  class='placeholder'>Placeholder</a></li>";
+            echo "</ul>";
+            echo "</li>";
+            echo "</ul> <!-- .cd-main-nav -->";
+            echo "</nav> <!-- .cd-main-nav-wrapper -->";
+        
+            echo "<a href='#0' class='cd-nav-trigger'><span></span></a>";
+          }
+          ?>
+
 	</header>
 	<!-- main content starts here -->
 	<main class="cd-main-content">
