de9b7dab1d8047cc98bf84a0cce043567850283a
Tom <tombeugel@gmail.com>
2019-11-09 00:29:36 +0100

    ik haat mijn leven!

--- account/resetpassword.php
+++ account/resetpassword.php
@@ -23,29 +23,17 @@
 	<?php
 	//Hier sturen we de password reset mail
 	
-	if (isset($_POST['email'])) {
+	if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['username']) && !empty($_POST['username'])) {
+		
 		echo "klaar";
 		//data schoonmaken
 		$safemail = mysqli_real_escape_string($dbconnection, $_POST['email']);
-		
-		//bestemming
-		$to = $safemail;
-		
-		//onderwerp
-		$subject = "Password recovery";
-		
-		//bericht
-		$sql = "select verificatiecode from emailverificatie where usermail=" . $safemail;
+		$safeusername = mysqli_real_escape_string($dbconnection, $_POST['username']);
+		$sql = "select usermail from user where usermail = '{$_POST['email']}';";
 		$result = $dbConnection->query($sql);
-		$vercode = $result->fetch_assoc();
-		$message = "Enter the following code in the website to reset your password: <br>" . $vercode['verificatiecode'];
-		
-		//verstuurder
-		$headers = "From: Reset@hbo-ictmemes.nl";
-		
-		
-		//versturen van bericht
-		mail($to, $subject, $message, headers);
+		if ($result) {
+			sendemailverification($safeusername, $safemail, "wachtwoordreset");
+		}
 	}
 	?>
 	
