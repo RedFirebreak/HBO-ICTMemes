e9404910845b06e7efef512f507c20fc61467152
Tom <tombeugel@gmail.com>
2019-11-11 13:23:08 +0100

    PUSH IT!

--- verify/form.resetpassword.php
+++ verify/form.resetpassword.php
@@ -21,25 +21,67 @@
     5. Informeer de user 
     -->
 
+<body>
+	<form action="/verify/index.php" method="post">
+		<p>Password<br>
+			<input type="password" id="newpassword" name="newpassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+				required></p>
+		<div class="col-md-6" id="message">
+			<p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+			<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+			<p id="number" class="invalid">A <b>number</b></p>
+			<p id="length" class="invalid">Minimum <b>8 characters</b></p>
+		</div>
 
-<form action="/verify/index.php" method="post">
-    <p>Password<br>
-        <input type="password" id="newpassword" name="newpassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-            required></p>
-    <div class="col-md-6" id="message">
-        <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-        <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-        <p id="number" class="invalid">A <b>number</b></p>
-        <p id="length" class="invalid">Minimum <b>8 characters</b></p>
-    </div>
 
+		<p>Confirm password<br>
+			<input type="password" name="newpasswordcheck" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></p>
 
-    <p>Confirm password<br>
-        <input type="password" name="newpasswordcheck" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></p>
+		<?php echo recaptchaform ();?>
+		<button type="submit" class="btn btn-primary" name="reset_password">Change Password</button>
+	</form>
+</body>
 
-    <?php echo recaptchaform ();?>
-    <button type="submit" class="btn btn-primary" name="reset_password">Change Password</button>
-</form>
-
+<?php
+	// First check if recaptcha was valid
+		if(isset($_POST['g-recaptcha-response'])){
+			$captcha=$_POST['g-recaptcha-response'];
+			$recaptcha = recaptchaverwerk($captcha);
+		} else {
+	// Well, captcha wasn't entered so the form wasn't touched. Stop the rest!
+		return;
+		}
+		if (!$recaptcha){
+			echo "<div class='alert alert-danger' role='alert'>";
+			echo "Recaptcha is niet ingevuld, niet correct of is verlopen. Probeer het nog eens.";
+			echo "</div>";
+			return;
+		}
+	//captcha check done!
+			
+	//checken of de data er is
+	if (isset($_POST['newpassword']) && !empty($_POST['newpassword']) && isset($_POST['newpasswordcheck']) && !empty($_POST['newpasswordcheck'])) {
+		$safepassword = mysql_real_escape_string($_POST['newpassword']);
+		$safepasscheck = mysql_real_escape_string($_POST['newpasswordcheck']);
+		//checken of de wachtwoorden goed zijn ingevuld
+		if ($safepass = $safepasscheck) {
+			//checken of de user-ID en verificatiecode overeenkomen
+			$sql = "select verificatiecode from emailverificatie where `user-ID`={$_GET['username']};"
+			$result = $dbConnection->query($sql);
+			$vercode = mysqli_fetch_assoc($result);
+			if ($vercode['verificatiecode']=$_GET['code']) {
+				$sql = "insert into user (`wachtwoord`) values ('{$safepassword}');";
+				echo "Succes! Het wachtwoord is veranderd.";
+			}
+		}
+		else {
+			echo "Zorg ervoor dat de inhoud van beide vakken met elkaar overeenkomt";
+		}
+		}
+		else {
+			echo "Zorg ervoor dat je beide vakken invult";
+		}
+		
+?>
 
 <!-- This file is going to be required on a page. No need to put ending or starting html tags! -->
--- verify/index.php
+++ verify/index.php
@@ -46,43 +46,8 @@
             if (isset($_GET['resetpassword'])) {
               require('form.resetpassword.php');
             }
-			// First check if recaptcha was valid
-			  if(isset($_POST['g-recaptcha-response'])){
-				$captcha=$_POST['g-recaptcha-response'];
-				$recaptcha = recaptchaverwerk($captcha);
-			  } else {
-				// Well, captcha wasn't entered so the form wasn't touched. Stop the rest!
-				return;
-			  }
-				if (!$recaptcha){
-				  echo "<div class='alert alert-danger' role='alert'>";
-				  echo "Recaptcha is niet ingevuld, niet correct of is verlopen. Probeer het nog eens.";
-				  echo "</div>";
-				  return;
-				}
-			//captcha check done!
 			
-			//checken of de data er is
-			if (isset($_POST['newpassword']) && !empty($_POST['newpassword']) && isset($_POST['newpasswordcheck']) && !empty($_POST['newpasswordcheck'])) {
-				$safepassword = mysql_real_escape_string($_POST['newpassword']);
-				$safepasscheck = mysql_real_escape_string($_POST['newpasswordcheck']);
-				//checken of de wachtwoorden goed zijn ingevuld
-				if ($safepass = $safepasscheck) {
-					//checken of de user-ID en verificatiecode overeenkomen
-					$sql = "select verificatiecode from emailverificatie where `user-ID`={$_GET['username']};"
-					$result = $dbConnection->query($sql);
-					$vercode = mysqli_fetch_assoc($result);
-					if ($vercode['verificatiecode']=$_GET['code']) {
-						$sql = "insert into user (`wachtwoord`) values ('{$safepassword}');";
-						echo "Succes! Het wachtwoord is veranderd.";
-					}
-				}
-				else {
-					echo "Zorg ervoor dat de inhoud van beide vakken met elkaar overeenkomt";
-			}
-			else {
-				echo "Zorg ervoor dat je beide vakken invult";
-			}
+			
           ?>
                 </div>
             </div>
