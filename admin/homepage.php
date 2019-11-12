a08491f8c8797f64b2d217630a1c08010be087cf
Tom <tombeugel@gmail.com>
2019-11-12 16:19:34 +0100

    admin homepage

--- admin/homepage.php
+++ admin/homepage.php
@@ -28,29 +28,108 @@
 <div class="row placeholders">
     <div class="col-xs-6 col-sm-3 placeholder text-center">
         <!-- <img src="#" class="center-block img-responsive img-circle" alt="Generic placeholder thumbnail">  THIS IS SO YOU CAN IMPORT AN IMAGE -->
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>Aantal users</h4>
+        <span class="text-muted">
+		<?php
+                // Set the query
+                  if ($LoggedinUserrole == 'admin') {
+                    $aantalquery = "SELECT COUNT(*) aantal FROM user WHERE schoolnaam='$LoggedinSchool'";
+                  }
+                  if ($LoggedinUserrole == 'uber-admin') {
+                    $aantalquery = "SELECT COUNT(*) aantal from user";
+                  }
+
+                  $aantalresult = mysqli_query($dbConnection, $aantalquery);
+                  $aantalrow = mysqli_fetch_assoc($aantalresult);
+                  $aantal = $aantalrow['aantal'];
+                  echo "<span class='text-muted'><h2>$aantal</h2></span>";
+              ?>
+		</span>
     </div>
     <div class="col-xs-6 col-sm-3 placeholder text-center">
         <!-- <img src="#" class="center-block img-responsive img-circle" alt="Generic placeholder thumbnail">  THIS IS SO YOU CAN IMPORT AN IMAGE -->
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>Aantal admins</h4>
+        <span class="text-muted">
+		<?php 
+					if ($LoggedinUserrole == 'admin') {
+						$query = "select count(username) from user where userrole in ('admin', 'uber-admin') and schoolnaam in ('$LoggedinSchool');";
+					}
+					if ($LoggedinUserrole == 'uber-admin') {
+						$query = "select count(username) from user where userrole in ('admin', 'uber-admin');";
+					}
+					$result = $dbConnection->query($query);
+					$whatevz = mysqli_fetch_assoc($result);
+					$aantal = $whatevz['count(username)'];
+					echo "<span class='text-muted'><h2>$aantal</h2></span>";
+				  ?>
+		</span>
     </div>
     <div class="col-xs-6 col-sm-3 placeholder text-center">
         <!-- <img src="#" class="center-block img-responsive img-circle" alt="Generic placeholder thumbnail">  THIS IS SO YOU CAN IMPORT AN IMAGE -->
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>Aantal comments</h4>
+        <span class="text-muted">
+		<?php
+                // Set the query
+                  if ($LoggedinUserrole == 'admin') {
+                    $aantalquery = "SELECT COUNT(*) aantal FROM comments 
+					inner join meme on comments.`meme-ID`=meme.`meme-ID`
+					WHERE meme.`school`='$LoggedinSchool'";
+                  }
+                  if ($LoggedinUserrole == 'uber-admin') {
+                    $aantalquery = "SELECT COUNT(*) aantal from comments";
+                  }
+
+                  $aantalresult = mysqli_query($dbConnection, $aantalquery);
+                  $aantalrow = mysqli_fetch_assoc($aantalresult);
+                  $aantal = $aantalrow['aantal'];
+                  echo "<span class='text-muted'><h2>$aantal</h2></span>";
+              ?>
+			  </span>
     </div>
     <div class="col-xs-6 col-sm-3 placeholder text-center">
         <!-- <img src="#" class="center-block img-responsive img-circle" alt="Generic placeholder thumbnail">  THIS IS SO YOU CAN IMPORT AN IMAGE -->
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>Aantal memes</h4>
+        <span class="text-muted">
+		<?php 
+					if ($LoggedinUserrole == 'uber-admin'){
+						$query = "select count(`meme-ID`) from meme";
+					}
+					else {
+						$query = "select count(`meme-ID`) from meme where school = '$LoggedinSchool'";
+					}
+					$result = $dbConnection->query($query);
+					$whatevz = mysqli_fetch_assoc($result);
+					$aantal = $whatevz['count(`meme-ID`)'];
+					echo "<span class='text-muted'><h2>$aantal</h2></span>";
+				  ?>
+		</span>
     </div>
+ <?php
+			if ($LoggedinUserrole == 'uber-admin'){
+				$query = "select schoolnaam from school order by 1";
+				$result = $dbConnection->query($query);
+				while ($record = mysqli_fetch_assoc($result))
+				{
+				  echo "<div class='col-xs-6 col-sm-3 placeholder text-center'>
+					<!-- <img src='#' class='center-block img-responsive img-circle' alt='Generic placeholder thumbnail'>  THIS IS SO YOU CAN IMPORT AN IMAGE -->
+					<h4>Aantal ".$record['schoolnaam']."-memes</h4>
+					<span class='text-muted'>";
+					   
+						$sql = "select count(`meme-ID`) from meme where school in ('{$record['schoolnaam']}');";
+						$dinges = $dbConnection->query($sql);
+						$whatevz = mysqli_fetch_assoc($dinges);
+						echo $whatevz['count(`meme-ID`)'];
+					  
+					echo "</span>
+					</div>";
+				}
+			}
+			?>
 </div>
 
 <hr>
 
-<h2 class="sub-header">Here's a table</h2>
+<!--<h2 class="sub-header">Here's a table</h2>
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
@@ -86,7 +165,7 @@
             </tr>
         </tbody>
     </table>
-</div>
+</div>-->
 
 
 </div>
